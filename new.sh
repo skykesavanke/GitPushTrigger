8eb8fd7183ff9c88f0b6364d73b66402dfe86514
skykesavanke <keerthana.kesavan.ciec@skytv.it>
2024-08-01 19:38:44 +0530

    Update new.sh

--- new.sh
+++ new.sh
@@ -1,3 +1,4 @@
+#!bin/bash
 echo "Hello Everyone, Here I am triggering the pipeline after pushing code to repo."
 echo "Hi"
 echo "hellllo"
@@ -5,3 +6,4 @@ echo "qwfqd"
 echo"gahdjw"
 echo "hgdhakj"
 echo "hdiwaSBA"
+echo "werqry"
