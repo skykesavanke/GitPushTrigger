2760287e354e21c8f26c26450e56659049778a5c
skykesavanke <keerthana.kesavan.ciec@skytv.it>
2024-08-01 20:30:01 +0530

    added

--- new.sh
+++ new.sh
@@ -1,7 +1,4 @@
 #!bin/bash
 echo "Hello Everyone, Here I am triggering the pipeline after pushing code to repo."
 echo "Hi"
-<<<<<<< HEAD
 echo "welcome"
-=======
->>>>>>> 4def7eb51786c24f2b8f1460ec4547f7e342c262
